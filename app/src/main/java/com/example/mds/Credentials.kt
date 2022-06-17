9b7efd6b70ed4a44fa6511ea55176063e95f78a6
RoscaAlexandru775 <80165063+RoscaAlexandru775@users.noreply.github.com>
2022-06-17 23:04:14 +0300

    Update Credentials.kt

--- app/src/main/java/com/example/mds/Credentials.kt
+++ app/src/main/java/com/example/mds/Credentials.kt
@@ -1,7 +1,7 @@
 package com.example.mds
 
 object Credentials {
-    const val EMAIL = "civicguardmds@gmail.com"
-    const val PASSWORD = "tngkosnylehfhfaa"
+    const val EMAIL = ""
+    const val PASSWORD = ""
 
 }
